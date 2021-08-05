617a65664cb4b009db6cd522df3696f7bd638ae9
Soemonewho2 <80385902+Soemonewho2@users.noreply.github.com>
2021-08-05 22:28:33 +0200

    Update card.py

--- apocolyptic-fighter/card.py
+++ apocolyptic-fighter/card.py
@@ -1,14 +1,5 @@
 import pygame, sys
 pygame.init()
-pygame.font.init()
-
-display = pygame.display.set_mode((1000, 800))
-pygame.display.set_caption('card test')
-card = pygame.rect.Rect(20, 750, 200, 300)
-card1 = pygame.rect.Rect(250, 750, 200, 300)
-card2 = pygame.rect.Rect(480, 750, 200, 300)
-card3 = pygame.rect.Rect(710, 750, 200, 300)
-font = pygame.font.SysFont('comicsans', 20, bold=True)
 
 class Cards(object):
     def __init__(self, rect, font):
@@ -34,32 +25,3 @@ class Cards(object):
                     self.rect.y += 250
                     self.isHovering = False
                 
-        
-                
-
-Card = Cards(rect=card, font=font)
-Card1 = Cards(card1, font)
-Card2 = Cards(card2, font)
-Card3 = Cards(card3, font)
-
-run = True
-while True:
-    display.fill((0,0,0))
-    Card.draw()
-    Card.hover()
-
-    Card1.draw()
-    Card1.hover()
-
-    Card2.draw()
-    Card2.hover()
-
-    Card3.draw()
-    Card3.hover()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            run = False
-            sys.exit()
-    pygame.display.update()
-        
